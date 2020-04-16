02e0efc5812449f1ed47251d415163681f1bdce0
Maruga82 <maruga82@gmail.com>
2020-04-16 15:03:13 +0200

    added autowired

--- src/main/java/it/contrader/converter/ProdottoConverter.java
+++ src/main/java/it/contrader/converter/ProdottoConverter.java
@@ -1,19 +1,20 @@
 package it.contrader.converter;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import it.contrader.dto.ProdottoDTO;
-
 import it.contrader.model.Prodotto;
 
 @Component
 public class ProdottoConverter extends AbstractConverter<Prodotto, ProdottoDTO> {
-
+	@Autowired
+	WishListConverter wishlistconverter;
 	@Override
 	public Prodotto toEntity(ProdottoDTO prodottoDTO) {
 		Prodotto prodotto = null;
 		if (prodottoDTO != null) {
-			prodotto = new Prodotto(prodottoDTO.getId(), prodottoDTO.getName(), prodottoDTO.getDescription(), prodottoDTO.getPrice(), prodottoDTO.getPriority());
+			prodotto = new Prodotto(prodottoDTO.getId(), prodottoDTO.getName(), prodottoDTO.getDescription(), prodottoDTO.getPrice(), prodottoDTO.getPriority(), wishlistconverter.toEntity(prodottoDTO.getWishlist()));
 		}
 		return prodotto;
 	}
@@ -22,7 +23,7 @@ public class ProdottoConverter extends AbstractConverter<Prodotto, ProdottoDTO> 
 	public ProdottoDTO toDTO(Prodotto prodotto) {
 		ProdottoDTO prodottoDTO = null;
 		if (prodotto != null) {
-			prodottoDTO = new ProdottoDTO(prodotto.getId(), prodotto.getName(), prodotto.getDescription(), prodotto.getPrice(), prodotto.getPriority());
+			prodottoDTO = new ProdottoDTO(prodotto.getId(), prodotto.getName(), prodotto.getDescription(), prodotto.getPrice(), prodotto.getPriority(), wishlistconverter.toDTO(prodotto.getWishlist()));
 		}
 		return prodottoDTO;
 	}
