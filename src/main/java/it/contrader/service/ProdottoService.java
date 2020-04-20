28c5ed7f4e4f06e39398204b755e530bf6e33460
Maruga82 <maruga82@gmail.com>
2020-04-20 15:56:50 +0200

    fixed bug

---
+++ src/main/java/it/contrader/service/ProdottoService.java
@@ -0,0 +1,11 @@
+package it.contrader.service;
+
+import org.springframework.stereotype.Service;
+
+import it.contrader.dto.ProdottoDTO;
+import it.contrader.model.Prodotto;
+
+@Service
+public class ProdottoService extends AbstractService<Prodotto, ProdottoDTO> {
+	
+}
