a92106a473b5b7f87db2f2db2f4db4c1fd7d456a
Francesco Pampallona <pampallonafrancesco@gmail.com>
2020-04-21 19:11:04 +0200

    wishlist added

---
+++ src/app/admin/wishlists/wishlists.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { UserDTO } from 'src/dto/userdto';
+import {WishListDTO} from 'src/dto/wishlistdto';
+import {WishListService} from 'src/service/wishlist.service';
+
+@Component({
+  selector: 'app-wishlists',
+  templateUrl: './wishlists.component.html',
+  styleUrls: ['./wishlists.component.css']
+})
+export class WishlistsComponent implements OnInit {
+  wishlists: WishListDTO[];
+  proprietario: UserDTO;
+  wishlisttoinsert: WishListDTO = new WishListDTO();
+
+  constructor(private service: WishListService ) { }
+
+  ngOnInit() {
+    this.proprietario = JSON.parse(localStorage.getItem('currentUser'));
+    this.getWishList();
+  }
+
+  getWishList(){
+    this.service.getAll().subscribe(wishlists => this.wishlists = wishlists)
+  }
+
+  delete(wishlist: WishListDTO){
+    this.service.delete(wishlist.id).subscribe(()=>this.getWishList());
+  }
+  
+  update(wishlist: WishListDTO){
+    this.service.update(wishlist).subscribe(()=>this.getWishList());
+  }
+
+  insert(wishlist: WishListDTO){
+    wishlist.proprietario = this.proprietario;
+    this.service.insert(wishlist).subscribe(()=>this.getWishList());
+  }
+
+  clear(){
+    this.wishlisttoinsert = new WishListDTO();
+  }
+
+}
---
+++ src/dto/wishlistdto.ts
@@ -0,0 +1,16 @@
+import { UserDTO } from './userdto';
+
+export class WishListDTO{
+    id: number;
+
+    name: string;
+
+    description: string;
+
+    proprietario: UserDTO;
+
+
+   
+    
+
+}
