7a9ee57edc8f85c9d314f3aa4035ac80e0c97d16
MicheleDA91 <michele.dellarciprete@hotmail.it>
2020-04-22 09:15:22 +0200

    bug fixed

--- src/app/admin/categories/categories.component.ts
+++ src/app/admin/categories/categories.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoryService } from 'src/service/category.service';
 import { CategoryDTO } from 'src/dto/categorydto';
+import { UserDTO } from 'src/dto/userdto';
+
 
 @Component({
   selector: 'app-categories',
@@ -11,12 +13,15 @@ export class CategoriesComponent implements OnInit {
 
   categories: CategoryDTO[];
   categorytoinsert: CategoryDTO = new CategoryDTO();
+  proprietario_c: UserDTO[];
 
   constructor(private service: CategoryService) { }
-
+   
+   
   ngOnInit() {
     this.getCategories();
-  }
+    this.proprietario_c = JSON.parse(localStorage.getItem('currentUser'));
+      }
   getCategories(){
     this.service.getAll().subscribe(categories => this.categories = categories);
       }
