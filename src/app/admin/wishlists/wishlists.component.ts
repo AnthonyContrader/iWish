22470b9705e87bd812e0696111f754d2f05c266c
Francesco Pampallona <pampallonafrancesco@gmail.com>
2020-04-30 21:40:31 +0200

    update

--- src/app/admin/wishlists/wishlists.component.ts
+++ src/app/admin/wishlists/wishlists.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { UserDTO } from 'src/dto/userdto';
-import {WishListDTO} from 'src/dto/wishlistdto';
-import {WishListService} from 'src/service/wishlist.service';
+import { WishListDTO } from 'src/dto/wishlistdto';
+import { ProdottoDTO } from 'src/dto/prodottodto';
+import { WishListService } from 'src/service/wishlist.service';
+import { ProdottoService } from 'src/service/prodotto.service';
 
 @Component({
   selector: 'app-wishlists',
@@ -10,37 +12,74 @@ import {WishListService} from 'src/service/wishlist.service';
 })
 export class WishlistsComponent implements OnInit {
   wishlists: WishListDTO[];
+  prodotti: ProdottoDTO[];
+  prodotto: ProdottoDTO;
   proprietario: UserDTO;
   wishlisttoinsert: WishListDTO = new WishListDTO();
 
-  constructor(private service: WishListService ) { }
+
+  constructor(private service: WishListService, private prodottoService: ProdottoService) { }
 
   ngOnInit() {
     this.proprietario = JSON.parse(localStorage.getItem('currentUser'));
     this.getWishList();
   }
 
-  
 
-  getWishList(){
-    this.service.getAll().subscribe(wishlists => this.wishlists = wishlists)
+
+  getWishList() {
+    this.service.getAll().subscribe(wishlists => {
+      this.wishlists = [];
+      for (let w of wishlists) {
+        if (w.proprietario.username === this.proprietario.username) {
+          this.wishlists.push(w);
+        }
+      }
+
+    });
   }
 
-  delete(wishlist: WishListDTO){
-    this.service.delete(wishlist.id).subscribe(()=>this.getWishList());
-  }
-  
-  update(wishlist: WishListDTO){
-    this.service.update(wishlist).subscribe(()=>this.getWishList());
+  getProdotti(w: WishListDTO) {
+    this.prodottoService.getAll().subscribe(products => {
+      this.prodotti = [];
+      for (let p of products) {
+        if (p.proprietario.username === this.proprietario.username) {
+          if (p.wishlist != null) {
+            if (p.wishlist.id === w.id) {
+              this.prodotti.push(p);
+            }
+          }
+        }
+      }
+    });
   }
 
-  insert(wishlist: WishListDTO){
+  remove(p: ProdottoDTO, w: WishListDTO){
+    p.wishlist = null;
+    this.prodotto = new ProdottoDTO();
+    this.prodotto = p;
+    
+    this.prodottoService.update(p).subscribe(()=>{
+      this.getProdotti(w);
+    });
+    
+  }
+
+  delete(wishlist: WishListDTO) {
+    this.service.delete(wishlist.id).subscribe(() => this.getWishList());
+  }
+
+  update(wishlist: WishListDTO) {
+    this.service.update(wishlist).subscribe(() => this.getWishList());
+  }
+
+  insert(wishlist: WishListDTO) {
     wishlist.proprietario = this.proprietario;
-    this.service.insert(wishlist).subscribe(()=>this.getWishList());
+    this.service.insert(wishlist).subscribe(() => this.getWishList());
     this.clear();
   }
 
-  clear(){
+  clear() {
     this.wishlisttoinsert = new WishListDTO();
   }
 
