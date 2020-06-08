66dc8efafaebe9dd052d9420e95a8a32e1b5218a
Francesco Pampallona <pampallonafrancesco@gmail.com>
2020-06-08 11:26:27 +0200

    bug fixed

--- src/app/layout/admin-layout/admin-layout.component.ts
+++ src/app/layout/admin-layout/admin-layout.component.ts
@@ -12,42 +12,16 @@ import {UserService} from 'src/service/user.service';
 
 
 export class AdminLayoutComponent implements OnInit {
-  user: UserDTO=new UserDTO();
-  portfolio: PortfolioDTO[]=[];
-  saldoAttuale: number = 1;
-  ciao: String="15";
-  proprietario: UserDTO;
-  
+
   
   constructor(private portfolioservice: PortfolioService, private userservice: UserService ) { 
     
   }
 
   ngOnInit() {
-  
-    this.proprietario = JSON.parse(localStorage.getItem('currentUser'));
-    this.getPortfolio();
-    console.log (this.saldoAttuale);
+
   }
-  getPortfolio() {
-    this.portfolioservice.getAll().subscribe(portafogli => {
-      this.portfolio=[] ;
-      
-     
-      let id=0;
-      this.saldoAttuale=0;
-      for(let p of portafogli){ 
-        if (p.proprietario_id===this.proprietario.id){
-          this.portfolio.push(p);
-         
-          if (p.id>id){ 
-            id=p.id;
-            this.saldoAttuale=p.totalmoney;
-          }
-        }
-      }
-   
-    });}
+  
 
 }
  
